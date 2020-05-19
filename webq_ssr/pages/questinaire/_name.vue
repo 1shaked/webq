7e912228705831b2ae59b3a707a9a09f0d287bef
shaked <shaked1hen@gmail.com>
2020-05-19 11:31:44 +0300

    added a id route

---
+++ webq_ssr/pages/questinaire/_name.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ questinaire.name }}
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  layout: 'questinaire',
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapState('Questinaire', [
+      'questinaire'
+    ])
+  }
+}
+</script>
