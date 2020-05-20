bded1d8e4665723964d1691a98d7c258b5340da7
shaked <shaked1hen@gmail.com>
2020-05-20 15:32:43 +0300

    seperate the create to components

--- webq_ssr/components/questinaire/Create/form/index.vue
+++ webq_ssr/components/questinaire/Create/form/index.vue
@@ -1,94 +1,24 @@
 <template>
   <div>
     <v-container>
-      <v-card
-        class="mx-auto my-12"
-        max-width="374"
-      >
-        <v-card-title>Cafe Badilico</v-card-title>
-
-        <v-card-text>
-          <v-row
-            align="center"
-            class="mx-0"
-          >
-            <v-rating
-              :value="4.5"
-              color="amber"
-              dense
-              half-increments
-              readonly
-              size="14"
-            />
-
-            <div class="grey--text ml-4">
-              4.5 (413)
-            </div>
-          </v-row>
-
-          <div class="my-4 subtitle-1">
-            $ • Italian, Cafe
-          </div>
-
-          <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-        </v-card-text>
-
-        <v-divider class="mx-4" />
-
-        <v-card-title>Tonight's availability</v-card-title>
-
-        <v-card-text>
-          <v-chip-group
-            active-class="deep-purple accent-4 white--text"
-            column
-          >
-            <v-chip>5:30PM</v-chip>
-
-            <v-chip>7:30PM</v-chip>
-
-            <v-chip>8:00PM</v-chip>
-
-            <v-chip>9:00PM</v-chip>
-          </v-chip-group>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-btn
-            color="deep-purple lighten-2"
-            text
-          >
-            Reserve
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-row>
+        <v-col cols="12" md="6">
+          <create-card />
+        </v-col>
+        <v-col cols="12" md="6">
+          <create-card />
+        </v-col>
+      </v-row>
     </v-container>
-    <div class="card">
-      <div class="title">
-        Standard
-      </div>
-
-      <div class="features">
-        <ul>
-          <li>
-            <span>
-              {{ questinaire.questions[0].name }}
-            </span>
-          </li>
-          <li>
-            {{ questinaire.questions[0].content }}
-          </li>
-          <li><span>3</span> Pages</li>
-          <li><span>1</span> Hour free support</li>
-        </ul>
-      </div><!--/features-->
-
-      <a href="#" class="btn">Check it out</a>
-    </div>
   </div>
 </template>
 <script>
 import { mapState } from 'vuex'
+const createCard = () => import('./createCard')
 export default {
+  components: {
+    'create-card': createCard
+  },
   computed: {
     ...mapState('Questinaire', [
       'questinaire'
