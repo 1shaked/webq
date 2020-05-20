9b0bb7241eec44b26d2ddcbaf3d7525b52ea0766
shaked <shaked1hen@gmail.com>
2020-05-20 15:31:53 +0300

    created the content types posible now

---
+++ webq_ssr/storeAssetes/contentTypes.js
@@ -0,0 +1,28 @@
+export default [{
+  text: 'כפתור בחירה',
+  value: {
+    type: 'question',
+    name: 'radio'
+  }
+},
+{
+  text: 'בחירה מרובה',
+  value: {
+    type: 'question',
+    name: 'check box'
+  }
+},
+{
+  text: 'שאלת טקסט',
+  value: {
+    type: 'question',
+    name: 'text'
+  }
+},
+{
+  text: 'כותרת',
+  value: {
+    type: 'content',
+    name: 'header'
+  }
+}]
