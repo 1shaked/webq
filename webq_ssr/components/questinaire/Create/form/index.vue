dd427885e255bf45264ed215a6cc462adf12fa19
shaked <shaked1hen@gmail.com>
2020-05-20 10:51:03 +0300

    added the card style and example

--- webq_ssr/components/questinaire/Create/form/index.vue
+++ webq_ssr/components/questinaire/Create/form/index.vue
@@ -1,10 +1,48 @@
 <template>
   <div>
-    questions list
+    {{ questinaire.questions[0] }}
+    <div class="card">
+      <div class="title">
+        Standard
+      </div>
+
+      <div class="icon">
+        <svg
+          id="Layer_1"
+          enable-background="new 0 0 512 512"
+          height="512px"
+          version="1.1"
+          viewBox="0 0 512 512"
+          width="512px"
+          xml:space="preserve"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        ><path d="M468.493,101.637L371.955,5.098H159.57v77.231H43.724v424.769h308.923v-77.231h115.846V101.637z   M371.955,32.401l69.236,69.235h-69.236V32.401z M63.031,487.79V101.637h173.769v96.538h96.538V487.79H63.031z M256.108,109.632  l69.236,69.235h-69.236V109.632z M352.647,410.56V178.867l-96.538-96.538h-77.231V24.406h173.769v96.538h96.538V410.56H352.647z" fill="#37404D" /></svg>
+      </div><!--/icon-->
+
+      <div class="features">
+        <ul>
+          <li><span>5</span> Edits</li>
+          <li><span>1GB</span> Storage</li>
+          <li><span>3</span> Pages</li>
+          <li><span>1</span> Hour free support</li>
+        </ul>
+      </div><!--/features-->
+
+      <a href="#" class="btn">Check it out</a>
+    </div>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
 export default {
-
+  computed: {
+    ...mapState('Questinaire', [
+      'questinaire'
+    ])
+  }
 }
 </script>
+<style lang="scss" scoped>
+@import '~/style/card.scss';
+</style>
