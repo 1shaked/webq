dc56f140dd1591a8c1f1f2dacff70a6cba37a380
shaked <shaked1hen@gmail.com>
2020-05-20 10:51:30 +0300

    deleted the exstra comma

--- webq_ssr/nuxt.config.js
+++ webq_ssr/nuxt.config.js
@@ -36,7 +36,7 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify', // not sure yet
+    '@nuxtjs/vuetify' // not sure yet
   ],
   /*
   ** Nuxt.js modules
