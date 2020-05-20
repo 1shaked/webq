2428cf6139f23105b627faf20b5bbd312cfcbaba
shaked <shaked1hen@gmail.com>
2020-05-20 11:30:22 +0300

    tring to fix the vetify plugin

--- webq_ssr/plugins/vuetify.js
+++ webq_ssr/plugins/vuetify.js
@@ -23,11 +23,59 @@
 //   ctx.$vuetify = vuetify.framework
 // }
 
+// import Vue from 'vue'
+// import Vuetify from 'vuetify'
+
+// Vue.use(Vuetify)
+
+// export default new Vuetify({
+//   rtl: true
+// })
+
+// import Vue from 'vue'
+// import Vuetify from 'vuetify'
+// import 'vuetify/dist/vuetify.min.css'
+// import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader version "^2.1.1" ,
+// Vue.use(Vuetify)
+
+// export default (ctx) => {
+//   const vuetify = new Vuetify({
+//     rtl: true,
+//     theme: {
+//       dark: false // From 2.0 You have to select the theme dark or light here
+//     }
+//   })
+
+//   ctx.app.vuetify = vuetify
+//   ctx.$vuetify = vuetify.framework
+// }
+
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
+import colors from 'vuetify/es5/util/colors'
 Vue.use(Vuetify)
 
-export default new Vuetify({
-  rtl: true
-})
+export default (ctx) => {
+  const vuetify = new Vuetify({
+    rtl: true,
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.deepPurple.lighten3,
+          accent: colors.deepPurple.accent3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent4
+        }
+      }
+    }
+  })
+
+  ctx.app.vuetify = vuetify
+  ctx.$vuetify = vuetify.framework
+}
