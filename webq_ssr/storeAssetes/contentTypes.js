49db2a782b07da528186cf0784412ae4c51a6aaa
shaked <shaked1hen@gmail.com>
2020-05-23 15:06:55 +0300

    cahnge the type so it is matching the components path

--- webq_ssr/storeAssetes/contentTypes.js
+++ webq_ssr/storeAssetes/contentTypes.js
@@ -2,27 +2,27 @@ export default [{
   text: 'כפתור בחירה',
   value: {
     type: 'question',
-    name: 'radio'
+    name: 'radioQuestion'
   }
 },
 {
   text: 'בחירה מרובה',
   value: {
     type: 'question',
-    name: 'select'
+    name: 'selectQuestion'
   }
 },
 {
   text: 'שאלת טקסט',
   value: {
     type: 'question',
-    name: 'text'
+    name: 'textQuestion'
   }
 },
 {
   text: 'כותרת',
   value: {
     type: 'content',
-    name: 'header'
+    name: 'headerContent'
   }
 }]
