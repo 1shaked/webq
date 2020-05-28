3d4542b162c43b299614930a8d542987f0664359
shaked <shaked1hen@gmail.com>
2020-05-28 18:34:13 +0300

    create the header component

--- webq_ssr/components/questinaire/Create/form/content/headerContent.vue
+++ webq_ssr/components/questinaire/Create/form/content/headerContent.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
-    <h1>header</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-text-field
+          label="הכותרת"
+          v-model="header"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
+  props: {
+    index: {
+      type: Number,
+      reqired: true,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      header: ''
+    }
+  }
 
 }
 </script>
