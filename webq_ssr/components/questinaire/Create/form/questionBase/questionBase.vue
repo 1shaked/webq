6417262f6820a65c42986cd4701830b3c323c908
shaked <shaked1hen@gmail.com>
2020-05-29 00:16:54 +0300

    the compoent that render for each question

---
+++ webq_ssr/components/questinaire/Create/form/questionBase/questionBase.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-text-field
+          label="שם השאלה"
+          placeholder="שאלה לדוגמא"
+          outlined
+        />
+      </v-col>
+      <v-col cols="12" md="8" lg="6">
+        <v-text-field
+          label="תוכן השאלה"
+          placeholder="מה הגיל שלך?"
+          outlined
+        />
+      </v-col>
+      <v-col cols="12" md="8" lg="6">
+        <v-text-field
+          label="מידע לדוגמא על השדה: מה ת.ז שלך"
+          placeholder="000-000-000"
+          outlined
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    index: {
+      type: Number,
+      default: 0,
+      reqired: true
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
