9159d8dc79faa0c3fed0c543d94b8ddf9f876454
shaked <shaked1hen@gmail.com>
2020-05-29 16:46:13 +0300

    created the base style component with the option of toggle the under line, bold option

--- webq_ssr/components/questinaire/Create/form/baseStyle.vue
+++ webq_ssr/components/questinaire/Create/form/baseStyle.vue
@@ -12,7 +12,7 @@
     </v-chip>
 
     <v-chip>
-      <v-avatar left>
+      <v-avatar left @click="ToggleBasaeStyle('underline', 'text-decoration')">
         <U>U</U>
         <!-- <v-icon>mdi-checkbox-marked-circle</v-icon> -->
       </v-avatar>
@@ -28,8 +28,41 @@
 </template>
 
 <script>
+import { mapMutations, mapGetters } from 'vuex'
 export default {
+  props: {
+    index: {
+      type: Number,
+      default: 0,
+      reqired: true
+    }
+  },
+  data () {
+    return {
 
+    }
+  },
+  computed: {
+    ...mapGetters('Questinaire', [
+      'questinaireContentByIndex'
+    ])
+  },
+  methods: {
+    ...mapMutations('Questinaire', [
+      'ADD_STYLE_TO_QUESTION'
+    ]),
+    ToggleBasaeStyle (action, parameter, replaceWith = '') {
+      const style = this.questinaireContentByIndex(this.index).style[parameter]
+      const addedAction = style ? style.replace(action, replaceWith) : action
+      this.ADD_STYLE_TO_QUESTION({
+        questionIndex: this.index,
+        styleObj: {
+          key: parameter,
+          value: addedAction
+        }
+      })
+    }
+  }
 }
 </script>
 
