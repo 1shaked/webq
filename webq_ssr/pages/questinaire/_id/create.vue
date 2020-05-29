a252d749331bd820e382558c035d0a3a48977915
shaked <shaked1hen@gmail.com>
2020-05-29 16:47:45 +0300

    added the control panel to the create [age and lasy loading

--- webq_ssr/pages/questinaire/_id/create.vue
+++ webq_ssr/pages/questinaire/_id/create.vue
@@ -5,18 +5,19 @@
         <create-questinaire />
       </v-col>
       <v-col cols="12" md="4">
-        לוח שליטה
-        {{ $route.params }}
+        <control-panel />
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
-import Create from '~/components/questinaire/create/index'
+const Create = () => import('~/components/questinaire/create/index')
+const ControlPanel = () => import('~/components/questinaire/controlPanel/index')
 export default {
   layout: 'questinaire',
   components: {
-    'create-questinaire': Create
+    'create-questinaire': Create,
+    'control-panel': ControlPanel
   }
 }
 </script>
