988fe5fa1029470541b5216d9755d4ef6e4c4ea8
shaked <shaked1hen@gmail.com>
2020-05-29 16:47:15 +0300

    creating the control panel to start create the preview

---
+++ webq_ssr/components/questinaire/controlPanel/controlPanelHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="display-3">
+    לוח שליטה
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- webq_ssr/components/questinaire/controlPanel/index.vue
+++ webq_ssr/components/questinaire/controlPanel/index.vue
@@ -1,15 +1,41 @@
 <template>
-  <div>
-    control panel
-  </div>
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+      >
+        <control-panel-header />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-container class="toggleDisplay">
+          preview
+        </v-container>
+      </v-col>
+      <v-col>
+        <v-container class="toggleEdit">
+          edit
+        </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+const controlPanelHeader = () => import('./controlPanelHeader')
 export default {
-
+  components: {
+    'control-panel-header': controlPanelHeader
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.toggleDisplay {
+  text-align: center;
+}
+.toggleEdit {
+  text-align: center;
+}
 </style>
