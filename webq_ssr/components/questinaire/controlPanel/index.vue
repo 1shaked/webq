0c083be648dc4b1dc451be84283190f5b516a501
shaked <shaked1hen@gmail.com>
2020-05-31 06:12:18 +0300

    adding the toggle display button

--- webq_ssr/components/questinaire/controlPanel/index.vue
+++ webq_ssr/components/questinaire/controlPanel/index.vue
@@ -9,9 +9,11 @@
     </v-row>
     <v-row>
       <v-col>
-        <v-container class="toggleDisplay">
-          preview
-        </v-container>
+        <v-btn>
+          <v-container class="toggleDisplay" @click="TOGGLE_VIEW_STATE">
+            preview
+          </v-container>
+        </v-btn>
       </v-col>
       <v-col>
         <v-container class="toggleEdit">
@@ -23,10 +25,16 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
 const controlPanelHeader = () => import('./controlPanelHeader')
 export default {
   components: {
     'control-panel-header': controlPanelHeader
+  },
+  methods: {
+    ...mapMutations('Questinaire', [
+      'TOGGLE_VIEW_STATE'
+    ])
   }
 }
 </script>
