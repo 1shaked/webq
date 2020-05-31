9d609573040873d7bfdb420d91dacc667180e220
shaked <shaked1hen@gmail.com>
2020-05-31 06:13:32 +0300

    adding the v-if to toglle display

--- webq_ssr/pages/questinaire/_id/create.vue
+++ webq_ssr/pages/questinaire/_id/create.vue
@@ -2,7 +2,8 @@
   <v-container class="bv-example-row">
     <v-row>
       <v-col cols="12" md="8">
-        <create-questinaire />
+        <create-questinaire v-if="view === 'edit'" />
+        <preview-questinaire v-if="view === 'preview'" />
       </v-col>
       <v-col cols="12" md="4">
         <control-panel />
@@ -11,13 +12,21 @@
   </v-container>
 </template>
 <script>
-const Create = () => import('~/components/questinaire/create/index')
-const ControlPanel = () => import('~/components/questinaire/controlPanel/index')
+import { mapState } from 'vuex'
+const create = () => import('~/components/questinaire/create/index')
+const controlPanel = () => import('~/components/questinaire/controlPanel/index')
+const preview = () => import('~/components/questinaire/preview/index')
 export default {
   layout: 'questinaire',
   components: {
-    'create-questinaire': Create,
-    'control-panel': ControlPanel
+    'create-questinaire': create,
+    'control-panel': controlPanel,
+    'preview-questinaire': preview
+  },
+  computed: {
+    ...mapState('Questinaire', [
+      'view'
+    ])
   }
 }
 </script>
