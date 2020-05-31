55116ca6f32f14d1397fbd6dd044e7f54baf4805
shaked <shaked1hen@gmail.com>
2020-05-31 06:22:25 +0300

    added the loop for the content maneger

--- webq_ssr/components/questinaire/preview/index.vue
+++ webq_ssr/components/questinaire/preview/index.vue
@@ -1,15 +1,23 @@
 <template>
   <v-container>
     index preview
-    <content-maneger />
+    <v-container>
+      <content-maneger v-for="index in questinaireContentItemsLen" :key="index" :index="index" />
+    </v-container>
   </v-container>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 const contentManeger = () => import('./contentManeger')
 export default {
   components: {
     'content-maneger': contentManeger
+  },
+  computed: {
+    ...mapGetters('Questinaire', [
+      'questinaireContentItemsLen'
+    ])
   }
 }
 </script>
