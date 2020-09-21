cc9e4f68da542ae860aba7200dc8681b52c39011
shaked chen <shaked1hen@gmail.com>
2020-09-21 12:10:10 +0300

    remove the use of mysql db

--- server/server/forms/post/submit.js
+++ server/server/forms/post/submit.js
@@ -6,57 +6,57 @@ const router = express.Router();
 
 
 
-let con = mysql.createConnection({
-    host: "localhost",
-    user: "shakedA",
-    password: "qwerty123456",
-    database: "webq"
-  });
-con.connect(function (err)
-{
-    if (err) throw err;
-});
+// let con = mysql.createConnection({
+//     host: "localhost",
+//     user: "shakedA",
+//     password: "qwerty123456",
+//     database: "webq"
+//   });
+// con.connect(function (err)
+// {
+//     if (err) throw err;
+// });
 
 
-router.post('/submit_form' , async (req , res) =>
-{
-    let form = req.body.form;
-    let name = form.name;
-    let answers = form.answers;
-    let time = new Date().toISOString().slice(0, 19).replace('T', ' ');    
-    //create this as the sql format f_name,q_name,ans,time
-    let sql_rows =[];
-    for (q_name in answers) 
-    {
-        //if the answer is an array when you choose multiplay answer
-        if ( Array.isArray(answers[q_name]) )
-        {
-            for (ans of answers[q_name])
-            {
-                sql_rows.push([name , q_name , ans , time]);        
-            }
-        }
-        else
-        {
-            sql_rows.push([name , q_name , answers[q_name] , time]);
-        }
-    }
+// router.post('/submit_form' , async (req , res) =>
+// {
+//     let form = req.body.form;
+//     let name = form.name;
+//     let answers = form.answers;
+//     let time = new Date().toISOString().slice(0, 19).replace('T', ' ');    
+//     //create this as the sql format f_name,q_name,ans,time
+//     let sql_rows =[];
+//     for (q_name in answers) 
+//     {
+//         //if the answer is an array when you choose multiplay answer
+//         if ( Array.isArray(answers[q_name]) )
+//         {
+//             for (ans of answers[q_name])
+//             {
+//                 sql_rows.push([name , q_name , ans , time]);        
+//             }
+//         }
+//         else
+//         {
+//             sql_rows.push([name , q_name , answers[q_name] , time]);
+//         }
+//     }
     
-    //submit to the database
-    let sql_querry = "INSERT INTO answers (form_name, question_name , answer , world_time) VALUES ?";
-    let format_string = mysql.format(sql_querry , [sql_rows]);
+//     //submit to the database
+//     let sql_querry = "INSERT INTO answers (form_name, question_name , answer , world_time) VALUES ?";
+//     let format_string = mysql.format(sql_querry , [sql_rows]);
     
-    con.query(format_string , (err, result, fields) =>
-    {
-        if (err) throw err;
+//     con.query(format_string , (err, result, fields) =>
+//     {
+//         if (err) throw err;
         
-        //sending the result
-        //res.sendStatus(201);
-        res.send(result);
-    });
-    //
-    //res.send(form);    
-});
+//         //sending the result
+//         //res.sendStatus(201);
+//         res.send(result);
+//     });
+//     //
+//     //res.send(form);    
+// });
 
 
 // let configConnection = {
