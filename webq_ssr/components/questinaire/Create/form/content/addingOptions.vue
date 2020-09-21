8c1fba90c1b3691c5051ff6dffd01b016b952911
shaked chen <shaked1hen@gmail.com>
2020-09-21 13:30:06 +0300

    creating the add option component

---
+++ webq_ssr/components/questinaire/Create/form/content/addingOptions.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-container>
+      <h2>Adding options</h2>
+      <v-row>
+        <v-col cols="12" lg="6">
+          <v-text-field
+            label="טקסט לבחירה"
+          />
+        </v-col>
+        <v-col cols="12" lg="6">
+          <v-text-field
+            label="תוכן התשובה"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
